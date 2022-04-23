89bd07fa9814c321e26e452de6874764d60df189
Yee Siang <exiang83@yahoo.com>
2022-04-23 18:01:00 +0000

    setup daisyui

---
+++ pages/daisyui.js
@@ -0,0 +1,6 @@
+
+export default function DaisyUI() {
+    return (
+        <button class="btn btn-primary">Button</button>
+    )
+  }
--- pages/tailwindcss.js
+++ pages/tailwindcss.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function TailwindCss() {
     return (
       <h1 className="text-3xl font-bold underline bg-amber-500">
         Hello world!
